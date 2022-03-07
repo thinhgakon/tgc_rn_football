6ab59eaf5f39298ffc52ba24f230d27b06482422
thinhgakon <toi.la.dan.xu.nghe@gmail.com>
2022-03-07 22:07:12 +0700

    Add screens: home, profile

---
+++ src/screens/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+
+const Home = () => {
+
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    );
+};
+
+export default Home;
---
+++ src/screens/profile/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+
+const Profile = () => {
+
+    return (
+        <View>
+            <Text>Profile Screen</Text>
+        </View>
+    );
+};
+
+export default Profile;
