77027587f6fe5ae7c5261e245075ce2f5437e406
thinhgakon <toi.la.dan.xu.nghe@gmail.com>
2022-03-07 21:48:35 +0700

    Thêm data cho constants

---
+++ src/constants/icons.js
@@ -0,0 +1,14 @@
+/**
+ * Define or change the images
+ */
+
+const home = require('assets/icons/home.png');
+const profile = require('assets/icons/profile.png');
+
+/** 
+ * export the images object
+ */
+export default {
+    home,
+    profile,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+import Fonts from './fonts';
+import Icons from './icons';
+import Images from './images';
+import Colors from './colors';
+
+export {Fonts, Icons, Images, Colors};
