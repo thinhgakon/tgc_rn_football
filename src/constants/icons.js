f01de41bddf0cc92f91fdf871e41767d1476193a
thinhgakon <toi.la.dan.xu.nghe@gmail.com>
2022-03-09 19:21:26 +0700

    Prepare the Fonts, Images, Colors, Icons, dummyData

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -4,6 +4,18 @@
 
 const home = require('./../../assets/icons/home.png');
 const profile = require('./../../assets/icons/profile.png');
+const Discover = require('./../../assets/icons/Discover.png');
+const Explore = require('./../../assets/icons/Explore.png');
+const Standings = require('./../../assets/icons/Standings.png');
+const More = require('./../../assets/icons/More.png');
+const Search = require('./../../assets/icons/Search.png');
+const Notification = require('./../../assets/icons/Notification.png');
+const Soccer = require('./../../assets/icons/Soccer.png');
+const Back = require('./../../assets/icons/Back.png');
+const Share = require('./../../assets/icons/Share.png');
+const Down = require('./../../assets/icons/Down.png');
+const Likes = require('./../../assets/icons/Likes.png');
+const Comments = require('./../../assets/icons/Comments.png');
 
 /** 
  * export the images object
@@ -11,4 +23,16 @@ const profile = require('./../../assets/icons/profile.png');
 export default {
     home,
     profile,
+    Discover,
+    Explore,
+    Standings,
+    More,
+    Search,
+    Notification,
+    Soccer,
+    Back,
+    Share,
+    Down,
+    Likes,
+    Comments
 };
