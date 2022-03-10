03c42c7fc0dc8583b0417d2c54ffbf68efb0e648
Thinh Nguyen Xuan <toi.la.dan.xu.nghe@gmail.com>
2022-03-10 16:25:59 +0700

    update Header UI

--- src/screens/discover/header.js
+++ src/screens/discover/header.js
@@ -30,6 +30,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
+        marginTop: 16,
     },
     headerText: {
         lineHeight: 35,
